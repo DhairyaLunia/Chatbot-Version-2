c06ca29cb5e509ad0ed76484047a2f97922a6134
DhairyaLunia <dhairyaluniawork@gmail.com>
2022-06-28 15:21:37 +0530

    first commit

---
+++ r1.js
@@ -0,0 +1,20 @@
+
+function ret()
+{
+document.getElementById("demo1").innerHTML="What IT services would you like?";
+document.getElementById("demo2").innerHTML="Web Development";
+document.getElementById("demo3").innerHTML="Android Development";
+}
+
+function ret3()
+{
+document.getElementById("demo4").innerHTML="Contact 9003098780 - Dhairya Lunia";
+
+}
+
+function ret2()
+{
+document.getElementById("demo5").innerHTML="What Financial services would you like?";
+document.getElementById("demo6").innerHTML="Accounts";
+document.getElementById("demo7").innerHTML="Graphs";
+}
